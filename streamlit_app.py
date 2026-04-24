e18706d79bce1472b7292717a2f1151f938adf7d
Lillian <31743351+lola1989@users.noreply.github.com>
2026-04-24 12:35:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,8 @@ session = cnx.session()
 
 # Show Fruit options from FRUIT_OPTIONS Table
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 # Convert Snowpart DF to a Pandas DF
 pd_df=mydata_frame.to_pandas()
