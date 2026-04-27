6bad83f15c503097ba60121ceb4ea0082578e16c
Lillian <31743351+lola1989@users.noreply.github.com>
2026-04-27 10:55:57 -0600

    Update kitchen_side_form.py

--- pages/kitchen_side_form.py
+++ pages/kitchen_side_form.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 
 # Write directly to the app
@@ -7,3 +6,30 @@ st.title(f":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
 st.write(
   """Orders that need to be filled.
   """)
+
+# Show Order_Filled options from DB
+my_dataframe = session.table("smoothies.public.orders") \
+    .filter(col("ORDER_FILLED") == 0) \
+    .collect()
+
+if my_dataframe:
+# make table editable
+    editable_df = st.data_editor(my_dataframe)
+    submitted = st.button('Submit')
+    
+    if submitted:
+    
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+
+        try:
+            og_dataset.merge(edited_dataset \
+                 , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID']) \
+                 , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})])
+            st.success('Order(s) Updated!', icon='👍')
+        except:
+            st.wtrit('Something went wrong.')
+            
+else:
+    st.success("There are no pending orders right now.", icon='👍')
+
